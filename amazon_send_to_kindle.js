f14952d929fa37c094b9747877db177bfc985af4
Mohit Aghera <mhaghera_alpha@yahoo.co.in>
2014-03-02 16:04:15 +0530

    Added selector class in the javascript, that allows amazon service to select the content.

--- amazon_send_to_kindle.js
+++ amazon_send_to_kindle.js
@@ -2,8 +2,8 @@
 	Drupal.behaviors.amazon_send_to_kindle = {
 		attach: function (context, settings) {
 			$(function(k){
-				window.$SendToKindle&&window.$SendToKindle.Widget?$SendToKindle.Widget.init({}):setTimeout(k,500);
-			});
+        window.$SendToKindle&&window.$SendToKindle.Widget?$SendToKindle.Widget.init({"title":".title","published":".submitted","content":".content","exclude":".field-name-field-specialities"}):setTimeout(k,500);
+			})();
 		}
 	};
 })(jQuery);
