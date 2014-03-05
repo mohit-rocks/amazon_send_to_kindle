3b840c3b5500d74c53d3207ac3b8761bf1b83986
Mohit Aghera <mhaghera_alpha@yahoo.co.in>
2014-03-05 22:23:08 +0530

    Added configuration form for admin, custom button generation code, styles for the button and button icon images.

--- amazon_send_to_kindle.js
+++ amazon_send_to_kindle.js
@@ -2,7 +2,7 @@
 	Drupal.behaviors.amazon_send_to_kindle = {
 		attach: function (context, settings) {
 			$(function(k){
-        window.$SendToKindle&&window.$SendToKindle.Widget?$SendToKindle.Widget.init({"title":".title", "author":".username", "published":".submitted","content":".content","exclude":".field-name-field-specialities"}):setTimeout(k,500);
+        window.$SendToKindle&&window.$SendToKindle.Widget?$SendToKindle.Widget.init({"title":".title", "author":".username", "published":".submitted","content":".content"}):setTimeout(k,500);
 			})();
 		}
 	};
